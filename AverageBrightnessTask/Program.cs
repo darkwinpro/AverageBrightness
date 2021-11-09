1d675847f46dc4222d1a1f13d962a5d4dc6584e3
Darkwin <Darkwin.pro@gmail.com>
2021-11-09 16:10:48 +0300

    Вычисление средней яркости всей картинки

---
+++ AverageBrightnessTask/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AverageBrightnessTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ImageHelper.ReadImage("photo.jpg",
+                out var redColor,
+                out var greenColor,
+                out var blueColor);
+
+            Console.WriteLine(GetAverageBrightness(redColor, greenColor, blueColor)); // 0.60989763300802
+        }
+
+        
+        public static double GetAverageBrightness(byte[,] red, byte[,] green, byte[,] blue)
+        {
+            // TODO: Напишите вашу реализацию этого метода
+            double brightness = 0;
+            for (var x = 0; x < red.GetLength(0); x++)
+            {
+                for (var y = 0; y < red.GetLength(1); y++)
+                {
+                    brightness += GetBrightnessPixel(red[x, y], green[x, y], blue[x, y]);
+                }
+            }
+
+            brightness = brightness / (red.GetLength(0) * red.GetLength(1));
+
+            return brightness;
+        }
+
+        public static double GetBrightnessPixel(byte red, byte green, byte blue)
+        {
+            // Формула вычисления яркости = (0.299*R + 0.587*G + 0.114*B) / 255
+            // TODO: Напишите вашу реализацию этого метода
+
+            double brightnessPixel = (0.299 * red + 0.587 * green + 0.114 * blue) / 255;
+            return brightnessPixel;
+        }
+    }
+}
---
+++ Test/Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AverageBrightness
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(0, 0, 0, 0, 0)]
+        [TestCase(255, 100, 100, 0.57, 0.6)]
+        [TestCase(255, 254, 255, 0.99, 1)]
+        [TestCase(255, 255, 255, 1, 1)]
+        [TestCase(100, 100, 100, 0.39, 0.4)]
+        [TestCase(200, 200, 200, 0.78, 0.8)]
+        [TestCase(200, 0, 100, 0.27, 0.3)]
+        public void WhenCountBrightnessPixel_AndDataIsCorrect_ThenBrightnessPixelCorrect(byte r, byte g,
+            byte b, double expectedResultMin, double expectedResultMax)
+        {
+            // Arrange. 
+
+            // Act. 
+            var actualResult = AverageBrightnessTask.Program.GetBrightnessPixel(r, g, b);
+            // Assert. 
+            actualResult.Should().BeInRange(expectedResultMin, expectedResultMax);
+        }
+
+        [Test]
+        public void WhenGetAverageBrightness_AndDataIsCorrect_ThenAverageBrightnessCorrect()
+        {
+            // Arrange. 
+            int width = 10;
+            int height = 10;
+
+            var red = new byte[width, height];
+            var green = new byte[width, height];
+            var blue = new byte[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    red[x, y] += (byte) (x + 15);
+                    green[x, y] += (byte) (34 + y);
+                    blue[x, y] += (byte) (10 + x);
+                }
+            }
+
+            // Act. 
+            var actualResult = AverageBrightnessTask.Program.GetAverageBrightness(red, green, blue);
+            // Assert. 
+            actualResult.Should().BeInRange(0.1, 0.2);
+        }
+    }
+}
